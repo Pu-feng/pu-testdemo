f9f45a0b0d8c54fffb1579dc5bceab7c26d80049
puwind <2542648484@qq.com>
2024-09-21 10:50:06 +0800

    feat: 支持github pages中使用history模式刷新404问题

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    copyIndexTo404() // 使用自定义插件
   ],
   base: './',
   resolve: {
@@ -17,3 +20,22 @@ export default defineConfig({
     }
   }
 })
+
+// 自定义插件，用于在构建后将 index.html 复制为 404.html
+function copyIndexTo404() {
+  // 用于解决 github pages 404 页面显示问题
+  return {
+    name: 'copy-index-to-404',
+    closeBundle() {
+      const distDir = path.resolve(__dirname, 'dist')
+      const indexFile = path.resolve(distDir, 'index.html')
+      const notFoundFile = path.resolve(distDir, '404.html')
+      if (fs.existsSync(indexFile)) {
+        fs.copyFileSync(indexFile, notFoundFile)
+        console.log('index.html has been copied to 404.html')
+      } else {
+        console.error('index.html does not exist in the dist directory')
+      }
+    }
+  }
+}
